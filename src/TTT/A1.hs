af5f7153f782249b349324b04b18ac2aaacc3bc5
eugene thoni <thonieugene@gmail.com>
2023-10-10 07:15:03 +0000

    modified:   src/TTT/A1.hs

--- src/TTT/A1.hs
+++ src/TTT/A1.hs
@@ -3,16 +3,16 @@ module TTT.A1 where
 import Data.Char (toUpper)
 
 -- Q#01
-
-_SIZE_ = undefined
+_SIZE_ :: Int
+_SIZE_ = 3
 
 -- Q#02
-
-_DISPLAY_LOGO_ = undefined
+_DISPLAY_LOGO_ :: Bool
+_DISPLAY_LOGO_ = True
 
 -- Q#03
-
-convertRowIndex = undefined
+convertRowIndex :: Char -> Int
+convertRowIndex rowValue = fromEnum(toUpper(rowValue)) - 60
 
 -- Q#04
 
